58b5c21bcddc8f07bb6fa6d1394d75d2638e3c08
Andrew Pelykh <andrew.pelykh@gmail.com>
2017-03-24 13:46:01 +0200

    Change assets.rb

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,7 @@ Bundler.require(*Rails.groups)
 
 module GetAutism
   class Application < Rails::Application
-  config.assets.enabled = false
+    config.assets.enabled = false
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
   end
