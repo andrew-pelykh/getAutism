e4fef3a8fdbedc455e7269af35123ab299b0b402
Andrew Pelykh <andrew.pelykh@gmail.com>
2017-04-03 13:50:17 +0300

    Last try to push to heroku

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  entry: ['./frontend/src/index.js'],
+  entry: ['./src/index.js'],
   output: {
-    path: './public/js',
+    path: '../public/js',
     filename: 'bundle.js'
   },
   module: {
